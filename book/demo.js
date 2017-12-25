28db9db289ef57b927fec3e4c561a5da10ede044
whjin <1414848321@qq.com>
2017-12-25 22:57:52 +0800

    push code

--- book/demo.js
+++ book/demo.js
@@ -1,9 +1,22 @@
 var koa = require('koa');
+var router = require('koa-router');
 var app = koa();
 
-app.use(function* () {
-    var path = this.path;
-    this.body = path;
+app.use(router(app));
+
+app.get('/', function* (next) {
+    this.body = '<p>this is a koa page.</p>'
+});
+
+app.param('id', function* (id, next) {
+    this.id = Number(id);
+    if (typeof this.id !== 'number') {
+        return this.status = 404;
+    }
+    yield next;
+}).get('/detail/:id', function* (next) {
+    var id = this.id;
+    this.body = id;
 });
 
 app.listen(3000);
